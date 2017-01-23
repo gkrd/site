a583196e100438fc6a465c8f501284543ac0fca3
Walker Hamilton <hey@walkerhamilton.com>
2017-01-23 09:11:53 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>St. Louis Gatekeepers Roller Derby</title>
+        <meta name="description" content="Gatekeepers Roller Derby is the premier men's roller derby association member-league from St. Louis, MO.">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+        <style>
+            body {
+                color: #06d91f;
+                background-color: #111;
+                background-image: url(/tmp_img/bg.png);
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            div.content {
+                max-width: 400px;
+                margin: 7% auto 0 auto;
+                text-align: center;
+                font-family: 'Montserrat', sans-serif;
+                text-transform: uppercase;
+            }
+            div.social svg {
+                width: 50px;
+                margin: 25px 15px 0 0;
+            }
+            hr {
+                border-color: #6b6b6b;
+            }
+            hr.two {
+                width: 50%;
+            }
+            div.content h1 {
+                font-size: 3em;
+                text-indent: -9999px;
+                background-image: url(/tmp_img/GK-Logo.png);
+                background-size: cover;
+                width: 300px;
+                height: 204px;
+                margin: 0 auto 30px auto;
+            }
+            div.content h2 {
+                font-size: 2em;
+            }
+            div.content h3 {
+                font-size: 1.5em;
+            }
+            body a:link,body a:visited, body a:active {
+                color: #06d91f;
+                text-decoration: none;
+            }
+            body a:hover {
+                color: #fff;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <h1>St. Louis GateKeepers Roller Derby</h1>
+            <hr class="one" />
+            <h2>Our new website is in training</h2>
+            <hr class="two" />
+            <h3>Keep up with us on social media 'til it's ready for game time</h3>
+            <hr class="three" />
+            <div class="social">
+                <a href="https://www.facebook.com/GateKeepersRollerDerby/" class="facebook"><svg version="1.1" id="Facebook" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 128 128" enable-background="new 0 0 128 128" xml:space="preserve">
+                <g>
+                	<circle id="facebook-back" fill="#06D91F" cx="64" cy="64" r="64"/>
+                	<path id="facebook-facebook" fill="#000000" d="M95.1367,29H32.8638C30.7295,29,29,30.729,29,32.8638v62.2729
+                		C29,97.2705,30.7295,99,32.8638,99h33.5249V71.8926h-9.1221v-10.565h9.1221v-7.7905c0-9.0415,5.5224-13.9648,13.5888-13.9648
+                		c3.8623,0,7.1827,0.2876,8.1504,0.4165v9.4487l-5.5927,0.0024c-4.3877,0-5.2364,2.0845-5.2364,5.1431v6.7446h10.461l-1.3623,10.565
+                		h-9.0987V99h17.8379C97.2705,99,99,97.2705,99,95.1367V32.8638C99,30.729,97.2705,29,95.1367,29z"/>
+                </g>
+                </svg>
+    </a>
+                <a href="https://twitter.com/stlgatekeepers" class="twitter"><svg version="1.1" id="Twitter" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 128 128" enable-background="new 0 0 128 128" xml:space="preserve">
+                <g>
+                	<circle id="twitter-back" fill="#06D91F" cx="64" cy="64" r="64"/>
+                	<path id="twitter-twitter" fill="#000000" d="M99.8398,41.7695c-2.6367,1.17-5.4707,1.96-8.4462,2.3155
+                		c3.0351-1.8204,5.3681-4.7022,6.4658-8.1363c-2.8408,1.6851-5.9883,2.9097-9.3379,3.5689
+                		c-2.6826-2.8584-6.5049-4.6436-10.7344-4.6436c-8.123,0-14.7065,6.584-14.7065,14.7051c0,1.1533,0.1303,2.2754,0.3808,3.3516
+                		c-12.2221-0.6133-23.0581-6.4678-30.311-15.3648c-1.2661,2.1714-1.9912,4.6978-1.9912,7.3931c0,5.1015,2.5962,9.6025,6.542,12.2397
+                		c-2.4107-0.0757-4.6783-0.7378-6.6607-1.8393c-0.0014,0.0615-0.0014,0.123-0.0014,0.1855c0,7.125,5.0693,13.0684,11.7968,14.4199
+                		c-1.2343,0.336-2.5337,0.5157-3.8745,0.5157c-0.9477,0-1.8691-0.0928-2.7671-0.2637c1.8716,5.8418,7.3023,10.0937,13.7378,10.2119
+                		c-5.0332,3.9453-11.374,6.2959-18.2646,6.2959c-1.187,0-2.357-0.0693-3.5073-0.2051c6.5083,4.1729,14.2377,6.6065,22.5429,6.6065
+                		c27.0498,0,41.8418-22.4082,41.8418-41.8418c0-0.6377-0.0146-1.2715-0.0429-1.9024C95.375,47.3086,97.8691,44.7188,99.8398,41.7695
+                		z"/>
+                </g>
+                </svg></a>
+                <a href="https://www.instagram.com/stlgatekeepers/" class="instagram"><svg version="1.1" id="Instagram" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 128 128" enable-background="new 0 0 128 128" xml:space="preserve">
+                <g>
+                	<circle id="instagram-back" fill="#06D91F" cx="64" cy="64" r="64"/>
+                	<path id="instagram-instagram" fill-rule="evenodd" clip-rule="evenodd" fill="#000000" d="M91.5713,58.4829h-6.2402
+                		c0.4541,1.7651,0.7236,3.6089,0.7236,5.5156c0,12.1831-9.875,22.0562-22.0552,22.0562c-12.1797,0-22.0542-9.8731-22.0542-22.0562
+                		c0-1.9067,0.2695-3.7505,0.7246-5.5156h-6.2407v30.3286c0,1.5205,1.2349,2.7549,2.7598,2.7549h49.6215
+                		c1.5254,0,2.7608-1.2344,2.7608-2.7549V58.4829z M91.5713,39.186c0-1.5219-1.2354-2.7568-2.7608-2.7568H80.542
+                		c-1.5225,0-2.7568,1.2349-2.7568,2.7568v8.272c0,1.522,1.2343,2.7568,2.7568,2.7568h8.2685c1.5254,0,2.7608-1.2348,2.7608-2.7568
+                		V39.186z M63.9995,50.2148c-7.6133,0-13.7847,6.1705-13.7847,13.7837c0,7.6128,6.1714,13.7867,13.7847,13.7867
+                		c7.6138,0,13.7857-6.1739,13.7857-13.7867C77.7852,56.3853,71.6133,50.2148,63.9995,50.2148 M91.5713,99.8398H36.4292
+                		c-4.5664,0-8.269-3.7041-8.269-8.2734V36.4292c0-4.5669,3.7026-8.269,8.269-8.269h55.1421c4.5674,0,8.2685,3.7021,8.2685,8.269
+                		v55.1372C99.8398,96.1357,96.1387,99.8398,91.5713,99.8398"/>
+                </g>
+                </svg></a>
+                <a href="https://www.youtube.com/channel/UCRYCwtGVhaHIWOpw6fGpnyQ" class="youtube"><svg version="1.1" id="YouTube" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 128 128" enable-background="new 0 0 128 128" xml:space="preserve">
+                <g>
+                	<circle id="youtube-back" fill="#06D91F" cx="64" cy="64" r="64"/>
+                	<path id="youtube-youtube" fill="#000000" d="M99.8398,78.3359c0,5.6309-4.6084,10.2403-10.2402,10.2403H38.3994
+                		c-5.6318,0-10.2392-4.6094-10.2392-10.2403V49.6641c0-5.6328,4.6074-10.2403,10.2392-10.2403h51.2002
+                		c5.6318,0,10.2402,4.6075,10.2402,10.2403V78.3359z M56.5967,72.793l19.3681-10.0352L56.5967,52.6523V72.793z"/>
+                </g>
+                </svg></a>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <br />
+            <small><a href="https://squareup.com/market/stl-gatekeepers">Shop for Merchandise</a></small>
+        </div>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-3906588-8']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+    </body>
+</html>
